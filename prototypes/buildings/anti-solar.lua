4e046d01ac6551764400629c078598485eb401ca
pyanodon <smylodon@gmail.com>
2021-04-29 11:41:58 -0300

    fixed solar

--- prototypes/buildings/anti-solar.lua
+++ prototypes/buildings/anti-solar.lua
@@ -24,7 +24,7 @@ ITEM {
 }
 
 ENTITY {
-    type = "solar-panel",
+    type = "assembling-machine",
     name = "anti-solar",
     icon = "__pyalternativeenergygraphics__/graphics/icons/anti-solar.png",
     icon_size = 64, icon_mipmaps = 4,
@@ -35,23 +35,66 @@ ENTITY {
     dying_explosion = "solar-panel-explosion",
     collision_box = {{-2.8, -2.8}, {2.8, 2.8}},
     selection_box = {{-3.0, -3.0}, {3.0, 3.0}},
-    --damaged_trigger_effect = hit_effects.entity(),
-    energy_source =
-    {
-      type = "electric",
-      usage_priority = "solar"
+    match_animation_speed_to_activity = false,
+    module_specification = {
+        module_slots = 0
     },
-    picture =
-    {
-      layers =
-      {
-        {
-          filename = "__pyalternativeenergygraphics__/graphics/icons/filler.png",
-          width = 4,
-          height = 4,
-          shift = util.by_pixel(-0, 0),
-        },
-      }
+    --allowed_effects = {"speed"},
+    crafting_categories = {"solar-tower"},
+    crafting_speed = 1,
+    energy_source = {
+        type = "electric",
+        usage_priority = "secondary-input",
+        emissions_per_minute = 0.0,
+    },
+    energy_usage = "400kW",
+    animation = {
+        layers = {
+            {
+                filename = "__pyalternativeenergygraphics__/graphics/entity/anti-solar/raw.png",
+                width = 192,
+                height = 192,
+                frame_count = 70,
+                line_length = 10,
+                animation_speed = 0.45,
+                --blend_mode = "additive",
+                --draw_as_glow = true,
+                shift = util.by_pixel(0, -0)
+            },
+            {
+              filename = "__pyalternativeenergygraphics__/graphics/entity/anti-solar/ao.png",
+              width = 192,
+              height = 192,
+              frame_count = 70,
+              line_length = 10,
+              animation_speed = 0.45,
+              --blend_mode = "multiplicative",
+              --draw_as_shadow = true,
+              shift = util.by_pixel(0, -0)
+            },
+            {
+              filename = "__pyalternativeenergygraphics__/graphics/entity/anti-solar/l.png",
+              width = 192,
+              height = 224,
+              frame_count = 70,
+              line_length = 10,
+              animation_speed = 0.45,
+              --blend_mode = "multiplicative",
+              draw_as_glow = true,
+              shift = util.by_pixel(0, -16)
+            },
+            {
+                filename = "__pyalternativeenergygraphics__/graphics/entity/anti-solar/sh.png",
+                width = 192,
+                height = 192,
+                frame_count = 70,
+                line_length = 10,
+                animation_speed = 0.45,
+                --blend_mode = "additive",
+                draw_as_shadow = true,
+                shift = util.by_pixel(16, -0)
+            },
+        }
     },
     vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
     production = "60kW",
