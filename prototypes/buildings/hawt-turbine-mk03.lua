7ed67b641feb129f67d01e1abbb37f0d4cab5681
pyanodon <smylodon@gmail.com>
2021-11-27 20:32:03 -0300

    balances

--- prototypes/buildings/hawt-turbine-mk03.lua
+++ prototypes/buildings/hawt-turbine-mk03.lua
@@ -81,7 +81,7 @@ ENTITY {
         output_flow_limit = "50MW",
         render_no_power_icon = false
     },
-    energy_production = '50MW',
+    energy_production = '150MW',
     energy_usage = "0kW",
     vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
     working_sound = {
