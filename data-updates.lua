a23a4f46d299377283e874061a9c583bd7bf3b96
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-02-05 05:09:45 -0500

    disable all combustion mix recipes

--- data-updates.lua
+++ data-updates.lua
@@ -17,87 +17,6 @@ if mods["pypetroleumhandling"] then
     require("prototypes/updates/pypetroleumhandling-updates")
 end
 
---disable combustion recipes--
-RECIPE('acetylene-combustion'):set_fields{hidden = true}
-RECIPE('coalgas-combustion'):set_fields{hidden = true}
-RECIPE('syngas-combustion'):set_fields{hidden = true}
-RECIPE('supercritical-combustion'):set_fields{hidden = true}
-RECIPE('ultrasupercritical-combustion'):set_fields{hidden = true}
-RECIPE('diesel-combustion'):set_fields{hidden = true}
-RECIPE('gasoline-combustion'):set_fields{hidden = true}
-RECIPE('heavy-oil-combustion'):set_fields{hidden = true}
-RECIPE('light-oil-combustion'):set_fields{hidden = true}
-RECIPE('methanol-combustion'):set_fields{hidden = true}
-RECIPE('olefin-combustion'):set_fields{hidden = true}
-RECIPE('combustion-olefin'):set_fields{hidden = true}
-RECIPE('petgas-combustion'):set_fields{hidden = true}
-RECIPE('refsyngas-combustion'):set_fields{hidden = true}
-RECIPE('hydrogen-combustion'):set_fields{hidden = true}
-RECIPE('lard-combustion'):set_fields{hidden = true}
-RECIPE('bio-oil-combustion'):set_fields{hidden = true}
-RECIPE('fuel-oil-combustion'):set_fields{hidden = true}
-RECIPE('natural-gas-combustion'):set_fields{hidden = true}
-RECIPE('aromatics-combustion'):set_fields{hidden = true}
-RECIPE('scrude-combustion'):set_fields{hidden = true}
-RECIPE('crude-oil-combustion'):set_fields{hidden = true}
-RECIPE('naphtha-combustion'):set_fields{hidden = true}
-RECIPE('tall-oil-combustion'):set_fields{hidden = true}
-RECIPE('coalslurry-combustion'):set_fields{hidden = true}
-RECIPE('ethanol-combustion'):set_fields{hidden = true}
-RECIPE('fish-oil-combustion'):set_fields{hidden = true}
-RECIPE('methane-combustion'):set_fields{hidden = true}
-RECIPE('oleochemical-combustion'):set_fields{hidden = true}
-RECIPE('diborane-combustion'):set_fields{hidden = true}
-RECIPE('fuelrodmk02-combustion'):set_fields{hidden = true}
-RECIPE('fuelrodmk03-combustion'):set_fields{hidden = true}
-RECIPE('fuelrodmk04-combustion'):set_fields{hidden = true}
-RECIPE('fuelrodmk05-combustion'):set_fields{hidden = true}
-RECIPE('kerosene-combustion'):set_fields{hidden = true}
-RECIPE('lubricant-combustion'):set_fields{hidden = true}
-RECIPE('xylenol-combustion'):set_fields{hidden = true}
-RECIPE('benzene-combustion'):set_fields{hidden = true}
-
---disable biomass combustion recipes--
-RECIPE('acetylene-combustion-biomass'):set_fields{hidden = true}
-RECIPE('coalgas-combustion-biomass'):set_fields{hidden = true}
-RECIPE('syngas-combustion-biomass'):set_fields{hidden = true}
-RECIPE('supercritical-combustion-biomass'):set_fields{hidden = true}
-RECIPE('ultrasupercritical-combustion-biomass'):set_fields{hidden = true}
-RECIPE('diesel-combustion-biomass'):set_fields{hidden = true}
-RECIPE('gasoline-combustion-biomass'):set_fields{hidden = true}
-RECIPE('heavy-oil-combustion-biomass'):set_fields{hidden = true}
-RECIPE('light-oil-combustion-biomass'):set_fields{hidden = true}
-RECIPE('methanol-combustion-biomass'):set_fields{hidden = true}
-RECIPE('olefin-combustion-biomass'):set_fields{hidden = true}
-RECIPE('combustion-olefin-biomass'):set_fields{hidden = true}
-RECIPE('petgas-combustion-biomass'):set_fields{hidden = true}
-RECIPE('refsyngas-combustion-biomass'):set_fields{hidden = true}
-RECIPE('hydrogen-combustion-biomass'):set_fields{hidden = true}
-RECIPE('lard-combustion-biomass'):set_fields{hidden = true}
-RECIPE('bio-oil-combustion-biomass'):set_fields{hidden = true}
-RECIPE('fuel-oil-combustion-biomass'):set_fields{hidden = true}
-RECIPE('natural-gas-combustion-biomass'):set_fields{hidden = true}
-RECIPE('aromatics-combustion-biomass'):set_fields{hidden = true}
-RECIPE('scrude-combustion-biomass'):set_fields{hidden = true}
-RECIPE('crude-oil-combustion-biomass'):set_fields{hidden = true}
-RECIPE('naphtha-combustion-biomass'):set_fields{hidden = true}
-RECIPE('tall-oil-combustion-biomass'):set_fields{hidden = true}
-RECIPE('coalslurry-combustion-biomass'):set_fields{hidden = true}
-RECIPE('ethanol-combustion-biomass'):set_fields{hidden = true}
-RECIPE('fish-oil-combustion-biomass'):set_fields{hidden = true}
-RECIPE('methane-combustion-biomass'):set_fields{hidden = true}
-RECIPE('oleochemical-combustion-biomass'):set_fields{hidden = true}
-RECIPE('diborane-combustion-biomass'):set_fields{hidden = true}
-RECIPE('fuelrodmk02-combustion-biomass'):set_fields{hidden = true}
-RECIPE('fuelrodmk03-combustion-biomass'):set_fields{hidden = true}
-RECIPE('fuelrodmk04-combustion-biomass'):set_fields{hidden = true}
-RECIPE('fuelrodmk05-combustion-biomass'):set_fields{hidden = true}
-RECIPE('kerosene-combustion-biomass'):set_fields{hidden = true}
-RECIPE('lubricant-combustion-biomass'):set_fields{hidden = true}
-RECIPE('xylenol-combustion-biomass'):set_fields{hidden = true}
-RECIPE('benzene-combustion-biomass'):set_fields{hidden = true}
-
-
 --MOVED RECIPES CATEGORIES
 RECIPE('aromatic-organic'):set_category("centrifuging")
 RECIPE('ash-separation'):set_category("centrifuging")
@@ -156,3 +75,20 @@ RECIPE("nv-center"):add_ingredient({type = "item", name = "mqdc", amount = 1}):a
 RECIPE("production-science-pack"):add_ingredient({type = "item", name = "low-density-structure", amount = 3}):add_ingredient({type = "item", name = "super-steel", amount = 5}):add_ingredient({type = "item", name = "gd-metal", amount = 1})
 
 ITEM("simik"):set_field{fuel_category = 'simik'}:set_field{fuel_value = '800MJ'}
+
+for r, recipe in pairs(data.raw.recipe) do
+    if recipe.category == "combustion" then
+        recipe.hidden = true
+        for t, tech in pairs(data.raw.technology) do
+            if tech.effects ~= nil then
+                for e, effect in pairs(tech.effects) do
+                    if effect.type == "unlock-recipe" then
+                        if effect.recipe == recipe.name then
+                            RECIPE(recipe.name):remove_unlock(tech.name)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
---
+++ prototypes/updates/base-updates.lua
@@ -0,0 +1,8 @@
+
+-- TECH CHANGES
+data.raw.technology['uranium-processing'].enabled = true
+data.raw.technology['uranium-processing'].hidden = false
+
+TECHNOLOGY('uranium-processing'):remove_pack('chemical-science-pack'):remove_prereq('chemical-science-pack')
+
+TECHNOLOGY('chemical-science-pack'):add_prereq('uranium-processing')
