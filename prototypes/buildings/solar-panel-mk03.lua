c1ce2ecf7b952b5bff7308f729212268764395bf
pyanodon <smylodon@gmail.com>
2022-02-27 18:41:13 -0300

    balance

--- prototypes/buildings/solar-panel-mk03.lua
+++ prototypes/buildings/solar-panel-mk03.lua
@@ -102,7 +102,7 @@ ENTITY{
         }
     },
     vehicle_impact_sound = {filename = '__base__/sound/car-metal-impact.ogg', volume = 0.65},
-    production = '3MW',
+    production = '4MW',
 }
 
 data:extend({
