664e83feb8e2e97bc394c2a5b2eb4116f592e249
ShadowGlass0 <gkecskem@gmail.com>
2022-03-07 10:48:39 +1100

    Bot fixes

--- data-final-fixes.lua
+++
@@ -1,2 +0,0 @@
-RECIPE('py-construction-robot-01'):add_ingredient({type = 'fluid', name = 'fish-oil', amount = 50})
-RECIPE('py-logistic-robot-01'):add_ingredient({type = 'fluid', name = 'fish-oil', amount = 50})
