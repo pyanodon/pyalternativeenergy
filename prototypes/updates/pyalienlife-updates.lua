92a9d1c4eecbe8c2d82278ff0059f735ff31a862
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-08-25 04:38:23 -0400

    moving nuclear 1 to chem

--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -28,6 +28,8 @@ TECHNOLOGY("kerogen"):add_pack("py-science-pack-4")
 TECHNOLOGY("railway-mk04"):add_pack("py-science-pack-4")
 TECHNOLOGY("fracking"):add_pack("py-science-pack-4")
 
+TECHNOLOGY("coated-container"):add_pack("py-science-pack-3")
+
 -- The zungror effect
 TECHNOLOGY("water-animals-mk04"):add_pack("military-science-pack")
 TECHNOLOGY("water-invertebrates-mk04"):add_pack("military-science-pack")
