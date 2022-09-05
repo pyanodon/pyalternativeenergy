dccabe27aff95c573423b6a035be425c48a9ad61
ShadowGlass0 <gkecskem@gmail.com>
2022-09-05 19:29:48 +1000

    rc-mk02 recipe

--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -165,8 +165,7 @@ RECIPE("seaweed-crop-mk02"):add_ingredient({type = "item", name = "self-assembly
 RECIPE("zungror-lair-mk02"):add_ingredient({type = "item", name = "self-assembly-monolayer", amount = 10}):add_ingredient { type = "item", name = "mechanical-parts-02", amount = 5 }
 RECIPE("fluid-drill-mk03"):add_ingredient({type = "item", name = "self-assembly-monolayer", amount = 5}):add_ingredient { type = "item", name = "mechanical-parts-02", amount = 1 }
 RECIPE("collector-mk02"):add_ingredient({type = "item", name = "self-assembly-monolayer", amount = 6}):add_ingredient { type = "item", name = "mechanical-parts-01", amount = 2 }
-RECIPE("rc-mk02"):add_ingredient({type = "item", name = "self-assembly-monolayer", amount = 12}):add_ingredient { type = "item", name = "mechanical-parts-01", amount = 10 }
-
+RECIPE("rc-mk02"):add_ingredient{"eva", 100}:add_ingredient({type = "item", name = "self-assembly-monolayer", amount = 12}):add_ingredient { type = "item", name = "mechanical-parts-01", amount = 10 }
 
 RECIPE("atomizer-mk03"):add_ingredient({type = "item", name = "ns-material", amount = 10}):add_ingredient { type = "item", name = "mechanical-parts-02", amount = 1 }
 RECIPE("bio-printer-mk03"):add_ingredient({type = "item", name = "ns-material", amount = 5}):add_ingredient({type = "item", name = "acrylic", amount = 40}):add_ingredient { type = "item", name = "mechanical-parts-02", amount = 1 }
