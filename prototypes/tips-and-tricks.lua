35e55bbae8560bccedec3954786b18d490dc06ae
Alex ten Brink <alextenbrink@gmail.com>
2023-01-18 19:12:03 +0100

    Added tips and tricks

---
+++ prototypes/tips-and-tricks.lua
@@ -0,0 +1,37 @@
+data:extend(
+{
+	{
+		type = "tips-and-tricks-item",
+		name = "pyalternativeenergy",
+		category = "wiki-py",
+		indent = 1,
+		order = "h",
+		is_title = true,
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "blimp",
+		category = "wiki-py",
+		indent = 2,
+		order = "ha",
+        tag = '[item=aerial-blimp-mk01]',
+        trigger =
+        {
+            type = "unlock-recipe",
+            recipe = "aerial-blimp-mk01"
+        },
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "nuclear-research",
+		category = "wiki-py",
+		indent = 2,
+		order = "hb",
+        tag = '[item=pu-238]',
+        trigger =
+        {
+            type = "unlock-recipe",
+            recipe = "nuclear-reactor"
+        },
+	},
+})
