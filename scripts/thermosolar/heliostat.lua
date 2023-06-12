15cabc900254fc2d2b87066635fd4de368c0561c
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-06-12 15:45:48 +0200

    Fix GUI crash when entity is not defined

--- scripts/thermosolar/heliostat.lua
+++ scripts/thermosolar/heliostat.lua
@@ -97,7 +97,7 @@ end
 Heliostat.events.on_gui_opened = function(event)
 	local player = game.get_player(event.player_index)
 	local entity = event.entity
-	if event.gui_type ~= defines.gui_type.entity or entity.name ~= 'solar-tower-building' then return end
+	if event.gui_type ~= defines.gui_type.entity or not entity or entity.name ~= 'solar-tower-building' then return end
 
 	local main_frame = player.gui.screen.add{type = 'frame', name = 'heliostat_gui', caption = entity.prototype.localised_name, direction = 'vertical'}
 	main_frame.tags = {unit_number = entity.unit_number}
