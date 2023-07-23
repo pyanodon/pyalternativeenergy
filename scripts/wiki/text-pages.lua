744590d1f5bb77c4ecc24ac8e0adedab89417711
notnotmelon <zacharypicco@gmail.com>
2023-07-23 13:11:21 -0500

    new wiki page for wind energy

--- scripts/wiki/text-pages.lua
+++ scripts/wiki/text-pages.lua
@@ -5,6 +5,12 @@ Wiki.events.on_init = function()
     remote.call('pywiki', 'add_section', 'alternativeenergy')
 
     remote.call('pywiki', 'add_page', {
+        name = 'wind',
+        section = 'alternativeenergy',
+        text_only = true
+    })
+
+    remote.call('pywiki', 'add_page', {
         name = 'blimp',
         section = 'alternativeenergy',
         text_only = true
