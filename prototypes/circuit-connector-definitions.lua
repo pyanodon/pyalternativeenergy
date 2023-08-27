9ca5ecef1b72fcdcd4cd3028ffe0352129c93a7e
Jake Staberg Morgan <47102583+JStMorgan@users.noreply.github.com>
2023-08-27 23:30:59 +0200

    Rare earth drill connectors (#32)
    * Added Rare-Earth drill circuit connectivity
    
    * Fixed shadow positions
    
    * Spelling fixes

--- prototypes/circuit-connector-definitions.lua
+++ prototypes/circuit-connector-definitions.lua
@@ -22,3 +22,14 @@ circuit_connector_definitions["geothermal-plant-mk01"] = circuit_connector_defin
     { variation = 26, main_offset = util.by_pixel(60, 155), shadow_offset = util.by_pixel(54, 167), show_shadow = false }
   }
 )
+
+circuit_connector_definitions["ree-mining-drill-mkxx"] = circuit_connector_definitions.create
+(
+  universal_connector_template,
+  {--Directions are up, right, down, left.
+    { variation = 26, main_offset = util.by_pixel(60, 32), shadow_offset = util.by_pixel(54, 44), show_shadow = false },
+    { variation = 26, main_offset = util.by_pixel(60, 32), shadow_offset = util.by_pixel(54, 44), show_shadow = false },
+    { variation = 26, main_offset = util.by_pixel(60, 32), shadow_offset = util.by_pixel(54, 44), show_shadow = false },
+    { variation = 26, main_offset = util.by_pixel(60, 32), shadow_offset = util.by_pixel(54, 44), show_shadow = false }
+  }
+)
