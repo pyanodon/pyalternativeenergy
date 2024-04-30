a8ad5c1c3a8366e0d68a40d24d4c234d6db78054
notnotmelon <zacharypicco@gmail.com>
2024-04-30 09:15:11 -0500

    https://github.com/pyanodon/pybugreports/issues/477

--- prototypes/fluids/hot-molten-salt.lua
+++ prototypes/fluids/hot-molten-salt.lua
@@ -8,7 +8,6 @@ FLUID {
     flow_color = {r = 1, g = 1, b = 1},
     max_temperature = 5000,
     heat_capacity = "1KJ",
-    gas_temperature = 15,
     gas_temperature = 5000,
     pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59,
