3fceaa3b6c7f31acf4d78b36518d85444b82194e
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-22 22:50:06 +0200

    fix collision_mask layers

--- prototypes/buildings/aerial-mk01.lua
+++ prototypes/buildings/aerial-mk01.lua
@@ -62,7 +62,7 @@ data:extend(
     immune_to_tree_impacts = true,
     has_belt_immunity = true,
     immune_to_rock_impacts = true,
-    collision_mask = {layers = {'not-colliding-with-itself'}},
+    collision_mask = {layers = {['not-colliding-with-itself'] = true}},
     selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
     attack_parameters =
     {
--- prototypes/buildings/hawt-turbine-mk04.lua
+++ prototypes/buildings/hawt-turbine-mk04.lua
@@ -36,7 +36,7 @@ local proto = ENTITY {
 	icon_size = 64,
     hidden = true,
     flags = {"placeable-neutral", "player-creation", },
-    collision_mask = {layers = {wind_layer, 'object-layer', 'player-layer', 'water-tile'}},
+    collision_mask = {layers = {[wind_layer] = true, ['object-layer'] = true, ['player-layer'] = true, ['water-tile'] = true}},
     minable = {mining_time = 0.5, result = "hawt-turbine-mk04"},
     placeable_by = {item = 'hawt-turbine-mk04', count = 1},
     fast_replaceable_group = "hawt-turbine",
@@ -132,7 +132,7 @@ data:extend(
         created_effect = {
             type = 'area',
             radius = 13.4,
-            collision_mask = {layers = {wind_layer}},
+            collision_mask = {layers = {[wind_layer] = true}},
             action_delivery = {
                 type = 'instant',
                 target_effects = {{
