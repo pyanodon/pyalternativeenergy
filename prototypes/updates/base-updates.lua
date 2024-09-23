e67374666f211747fa163953c97527bd56cb1ff6
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-23 20:52:01 +0200

    add ammo_category

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -1002,6 +1002,7 @@ data:extend({
 		name = "nuclear-artillery-shell",
 		icon = "__pyalternativeenergygraphics__/graphics/icons/fatmanbomb_64.png",
 		icon_size = 64,
+    ammo_category = "artillery",
 		ammo_type =
 		{
 		  category = "artillery-shell",
