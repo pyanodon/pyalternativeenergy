4689abbfea7122cb8a1ab5aa36a7cb5a4de8da2f
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-10-05 17:19:34 +0200

    fix entity graphics

--- prototypes/buildings/accumulator-mk03.lua
+++ prototypes/buildings/accumulator-mk03.lua
@@ -48,120 +48,122 @@ ENTITY {
         input_flow_limit = "100000kW",
         output_flow_limit = "100000kW"
     },
-    picture = {
-        layers = {
-            {
-                filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/raw.png",
-                width = 160,
-                height = 287,
-                frame_count = 1,
-                line_length = 1,
-                --blend_mode = "additive",
-                --draw_as_glow = true,
-                shift = util.by_pixel(0, -64)
-            },
-            {
-                filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/idle.png",
-                width = 160,
-                height = 256,
-                frame_count = 1,
-                line_length = 1,
-                --blend_mode = "additive",
-                draw_as_glow = true,
-                shift = util.by_pixel(0, -112)
-            },
-            {
-              filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/sh.png",
-              width = 192,
-              height = 160,
-              frame_count = 1,
-              line_length = 1,
-              --blend_mode = "multiplicative",
-              draw_as_shadow = true,
-              shift = util.by_pixel(16, -0)
-            },
-        }
-    },
-    charge_animation = {
-        layers = {
-            {
-                filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/raw.png",
-                width = 160,
-                height = 287,
-                frame_count = 1,
-                repeat_count = 25,
-                line_length = 1,
-                animation_speed = 0.3,
-                --blend_mode = "additive",
-                --draw_as_glow = true,
-                shift = util.by_pixel(0, -64)
-            },
-            {
-                filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/charging.png",
-                width = 160,
-                height = 256,
-                line_length = 5,
-                frame_count = 25,
-                shift = util.by_pixel(0, -112),
-                draw_as_glow = true,
-                animation_speed = 0.3
-            },
-            {
-                filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/sh.png",
-                width = 192,
-                height = 160,
-                repeat_count = 25,
-                frame_count = 1,
-                line_length = 1,
-                animation_speed = 0.3,
-                --blend_mode = "multiplicative",
-                draw_as_shadow = true,
-                shift = util.by_pixel(16, -0)
-              },
-        },
-    },
-    charge_cooldown = 30,
-    charge_light = {intensity = 1, size = 12, color = {r = 0.219, g = 0.835, b = 0.188}},
-    discharge_animation = {
-        layers = {
-            {
-                filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/raw.png",
-                width = 160,
-                height = 287,
-                frame_count = 1,
-                line_length = 1,
-                repeat_count = 25,
-                animation_speed = 0.3,
-                --blend_mode = "additive",
-                --draw_as_glow = true,
-                shift = util.by_pixel(0, -64)
-            },
-            {
-                filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/discharging.png",
-                width = 160,
-                height = 256,
-                line_length = 5,
-                frame_count = 25,
-                shift = util.by_pixel(0, -112),
-                draw_as_glow = true,
-                animation_speed = 0.3,
-            },
-            {
+    chargable_graphics = {
+        picture = {
+            layers = {
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/raw.png",
+                    width = 160,
+                    height = 287,
+                    frame_count = 1,
+                    line_length = 1,
+                    --blend_mode = "additive",
+                    --draw_as_glow = true,
+                    shift = util.by_pixel(0, -64)
+                },
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/idle.png",
+                    width = 160,
+                    height = 256,
+                    frame_count = 1,
+                    line_length = 1,
+                    --blend_mode = "additive",
+                    draw_as_glow = true,
+                    shift = util.by_pixel(0, -112)
+                },
+                {
                 filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/sh.png",
                 width = 192,
                 height = 160,
                 frame_count = 1,
                 line_length = 1,
-                repeat_count = 25,
-                animation_speed = 0.3,
                 --blend_mode = "multiplicative",
                 draw_as_shadow = true,
                 shift = util.by_pixel(16, -0)
-              },
+                },
+            }
         },
+        charge_animation = {
+            layers = {
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/raw.png",
+                    width = 160,
+                    height = 287,
+                    frame_count = 1,
+                    repeat_count = 25,
+                    line_length = 1,
+                    animation_speed = 0.3,
+                    --blend_mode = "additive",
+                    --draw_as_glow = true,
+                    shift = util.by_pixel(0, -64)
+                },
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/charging.png",
+                    width = 160,
+                    height = 256,
+                    line_length = 5,
+                    frame_count = 25,
+                    shift = util.by_pixel(0, -112),
+                    draw_as_glow = true,
+                    animation_speed = 0.3
+                },
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/sh.png",
+                    width = 192,
+                    height = 160,
+                    repeat_count = 25,
+                    frame_count = 1,
+                    line_length = 1,
+                    animation_speed = 0.3,
+                    --blend_mode = "multiplicative",
+                    draw_as_shadow = true,
+                    shift = util.by_pixel(16, -0)
+                },
+            },
+        },
+        charge_cooldown = 30,
+        charge_light = {intensity = 1, size = 12, color = {r = 0.219, g = 0.835, b = 0.188}},
+        discharge_animation = {
+            layers = {
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/raw.png",
+                    width = 160,
+                    height = 287,
+                    frame_count = 1,
+                    line_length = 1,
+                    repeat_count = 25,
+                    animation_speed = 0.3,
+                    --blend_mode = "additive",
+                    --draw_as_glow = true,
+                    shift = util.by_pixel(0, -64)
+                },
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/discharging.png",
+                    width = 160,
+                    height = 256,
+                    line_length = 5,
+                    frame_count = 25,
+                    shift = util.by_pixel(0, -112),
+                    draw_as_glow = true,
+                    animation_speed = 0.3,
+                },
+                {
+                    filename = "__pyalternativeenergygraphics__/graphics/entity/accumulator-mk03/sh.png",
+                    width = 192,
+                    height = 160,
+                    frame_count = 1,
+                    line_length = 1,
+                    repeat_count = 25,
+                    animation_speed = 0.3,
+                    --blend_mode = "multiplicative",
+                    draw_as_shadow = true,
+                    shift = util.by_pixel(16, -0)
+                },
+            },
+        },
+        discharge_cooldown = 60,
+        discharge_light = {intensity = 1, size = 12, color = {r = 0.862, g = 0.117, b = 0.117}},
     },
-    discharge_cooldown = 60,
-    discharge_light = {intensity = 1, size = 12, color = {r = 0.862, g = 0.117, b = 0.117}},
     vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact-1.ogg", volume = 0.65},
     working_sound = {
         sound = {
