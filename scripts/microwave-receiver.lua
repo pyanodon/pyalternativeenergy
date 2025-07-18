604489db6b52733365aad19c1619b18085180a63
Zachary Picco <zacharypicco@gmail.com>
2025-07-17 22:46:22 -0500

    Fixed Microwave receiver GUI not opening on base 2.0.60 stable. Resolves https://github.com/pyanodon/pybugreports/issues/1105

--- scripts/microwave-receiver.lua
+++ scripts/microwave-receiver.lua
@@ -1,65 +1,51 @@
-Microwave_Receiver = {}
-Microwave_Receiver.events = {}
-
-Microwave_Receiver.max_satellites_per_receiver = 15
-Microwave_Receiver.power_production_per_satellite = 80000
+local MAX_SATELLITES_PER_RECIEVER = 15
+local POWER_PRODUCTION_PER_SATELLITE_WATTS = 80000
 
 py.on_event(py.events.on_init(), function()
     storage.microwave_receivers = storage.microwave_receivers or {}
 end)
 
-function Microwave_Receiver.recalc_satellite_distribution(force)
-    local total_launched = force.get_item_launched("microwave-satellite")
-    local total_receivers = 0
-    for _, microwave_data in pairs(storage.microwave_receivers) do
-        if microwave_data.force == force then total_receivers = total_receivers + 1 end
-    end
-    local average_satellites = math.min(Microwave_Receiver.max_satellites_per_receiver, math.ceil(total_launched / total_receivers))
+local function get_gui(player)
+    return player.gui.screen.microwave_receiver_gui
+end
 
+local function update_gui(gui)
+    local force = game.forces[gui.tags.force]
+    if not force or not force.valid then return end
+
+    local microwave_data = storage.microwave_receivers[gui.tags.unit_number]
+    if not microwave_data then
+        gui.destroy(); return
+    end
+    
+    local content_flow = gui.content_frame.content_flow
+    content_flow.progressbar.value = microwave_data.allocated_satellites / MAX_SATELLITES_PER_RECIEVER
+    content_flow.progressbar.caption = {
+        "",
+        microwave_data.allocated_satellites .. "/" .. MAX_SATELLITES_PER_RECIEVER,
+        "    ",
+        py.format_energy(microwave_data.allocated_satellites * POWER_PRODUCTION_PER_SATELLITE_WATTS, "W")
+    }
+    content_flow.total_launched.caption = {"microwave-receiver-gui.launched-satellites", force.get_item_launched("microwave-satellite")}
+
+    local total_built = 0
     for _, microwave_data in pairs(storage.microwave_receivers) do
-        if total_launched >= average_satellites then
-            microwave_data.allocated_satellites = average_satellites
-            total_launched = total_launched - average_satellites
+        if microwave_data.force == force then total_built = total_built + 1 end
+    end
+    content_flow.total_built.caption = {"microwave-receiver-gui.total-receivers", total_built}
+end
+
+local function create_gui(player, entity)
+    -- check for existing ui and delete.
+    local old_gui = get_gui(player)
+    if old_gui then
+        if old_gui.tags.unit_number == entity.unit_number then
+            player.opened = old_gui
+            return
         else
-            microwave_data.allocated_satellites = total_launched
-            total_launched = 0
-        end
-
-        local entity = microwave_data.entity
-        entity.power_production = microwave_data.allocated_satellites * Microwave_Receiver.power_production_per_satellite
-        entity.electric_buffer_size = entity.power_production
-    end
-
-    for _, player in pairs(game.connected_players) do
-        if player.force == force then
-            local gui = player.gui.screen.microwave_receiver_gui
-            if gui then Microwave_Receiver.update_gui(gui) end
+            old_gui.destroy()
         end
     end
-end
-
-Microwave_Receiver.events.on_built = function(event)
-    local entity = event.entity
-    if entity.name ~= "microwave-receiver" then return end
-    storage.microwave_receivers[entity.unit_number] = {unit_number = entity.unit_number, entity = entity, allocated_satellites = 0, force = entity.force}
-    Microwave_Receiver.recalc_satellite_distribution(entity.force)
-end
-
-Microwave_Receiver.events.on_destroyed = function(event)
-    local entity = event.entity
-    if entity.name ~= "microwave-receiver" then return end
-    storage.microwave_receivers[entity.unit_number] = nil
-    Microwave_Receiver.recalc_satellite_distribution(entity.force)
-end
-
-Microwave_Receiver.events.on_rocket_launched = function(event)
-    Microwave_Receiver.recalc_satellite_distribution(event.rocket.force)
-end
-
-Microwave_Receiver.events.on_gui_opened = function(event)
-    local player = game.get_player(event.player_index)
-    local entity = event.entity
-    if event.gui_type ~= defines.gui_type.entity or not entity or entity.name ~= "microwave-receiver" then return end
 
     local main_frame = player.gui.screen.add {type = "frame", name = "microwave_receiver_gui", caption = entity.prototype.localised_name, direction = "vertical"}
     main_frame.style.width = 340
@@ -88,44 +74,82 @@ Microwave_Receiver.events.on_gui_opened = function(event)
     camera.zoom = 0.5
 
     content_flow.add {type = "progressbar", name = "progressbar", style = "electric_satisfaction_statistics_progressbar"}.style.horizontally_stretchable = true
-
     content_flow.add {type = "line"}
-
     content_flow.add {type = "label", name = "total_launched"}
     content_flow.add {type = "label", name = "total_built"}
 
-    Microwave_Receiver.update_gui(main_frame)
+    update_gui(main_frame)
 end
 
-Microwave_Receiver.events.on_gui_closed = function(event)
+local function recalc_satellite_distribution(force)
+    local total_launched = force.get_item_launched("microwave-satellite")
+    local total_receivers = 0
+    for _, microwave_data in pairs(storage.microwave_receivers) do
+        if microwave_data.force == force then total_receivers = total_receivers + 1 end
+    end
+    local average_satellites = math.min(MAX_SATELLITES_PER_RECIEVER, math.ceil(total_launched / total_receivers))
+
+    for _, microwave_data in pairs(storage.microwave_receivers) do
+        if total_launched >= average_satellites then
+            microwave_data.allocated_satellites = average_satellites
+            total_launched = total_launched - average_satellites
+        else
+            microwave_data.allocated_satellites = total_launched
+            total_launched = 0
+        end
+
+        local entity = microwave_data.entity
+        entity.power_production = microwave_data.allocated_satellites * POWER_PRODUCTION_PER_SATELLITE_WATTS
+        entity.electric_buffer_size = entity.power_production
+    end
+
+    for _, player in pairs(game.connected_players) do
+        if player.force == force then
+            local gui = get_gui(player)
+            if gui then update_gui(gui) end
+        end
+    end
+end
+
+py.on_event(py.events.on_built(), function(event)
+    local entity = event.entity
+    if entity.name ~= "microwave-receiver" then return end
+    storage.microwave_receivers[entity.unit_number] = {unit_number = entity.unit_number, entity = entity, allocated_satellites = 0, force = entity.force}
+    recalc_satellite_distribution(entity.force)
+end)
+
+py.on_event(py.events.on_destroyed(), function(event)
+    local entity = event.entity
+    if entity.name ~= "microwave-receiver" then return end
+    storage.microwave_receivers[entity.unit_number] = nil
+    recalc_satellite_distribution(entity.force)
+end)
+
+py.on_event(defines.events.on_rocket_launched, function(event)
+    recalc_satellite_distribution(event.rocket.force)
+end)
+
+py.on_event(py.events.on_entity_clicked(), function(event)
+    local player = game.get_player(event.player_index)
+    local entity = player.selected
+    if not entity or not entity.valid then return end
+    if entity.name ~= "microwave-receiver" then return end
+    create_gui(player, entity)
+end)
+
+py.on_event(defines.events.on_gui_opened, function(event)
+    if event.gui_type ~= defines.gui_type.entity then return end
+    local player = game.get_player(event.player_index)
+    local entity = event.entity
+    if not entity or not entity.valid then return end
+    if entity.name ~= "microwave-receiver" then return end
+    create_gui(player, entity)
+end)
+
+py.on_event({defines.events.on_gui_closed, defines.events.on_player_changed_surface}, function(event)
     local player = game.get_player(event.player_index)
     if (event.gui_type or player.opened_gui_type) == defines.gui_type.custom then
-        local gui = player.gui.screen.microwave_receiver_gui
+        local gui = get_gui(player)
         if gui then gui.destroy() end
     end
-end
-
-function Microwave_Receiver.update_gui(gui)
-    local force = game.forces[gui.tags.force]
-    if not force or not force.valid then return end
-    local microwave_data = storage.microwave_receivers[gui.tags.unit_number]
-    if not microwave_data then
-        gui.destroy(); return
-    end
-    local content_flow = gui.content_frame.content_flow
-
-    content_flow.progressbar.value = microwave_data.allocated_satellites / Microwave_Receiver.max_satellites_per_receiver
-    content_flow.progressbar.caption = {
-        "",
-        microwave_data.allocated_satellites .. "/" .. Microwave_Receiver.max_satellites_per_receiver,
-        "    ",
-        py.format_energy(microwave_data.allocated_satellites * Microwave_Receiver.power_production_per_satellite, "W")
-    }
-    content_flow.total_launched.caption = {"microwave-receiver-gui.launched-satellites", force.get_item_launched("microwave-satellite")}
-
-    local total_built = 0
-    for _, microwave_data in pairs(storage.microwave_receivers) do
-        if microwave_data.force == force then total_built = total_built + 1 end
-    end
-    content_flow.total_built.caption = {"microwave-receiver-gui.total-receivers", total_built}
-end
+end)
