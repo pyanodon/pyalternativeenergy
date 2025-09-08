0c9b5cf4eef868faba1d0ac5edacf95bd65f6dbb
Zachary Picco <zacharypicco@gmail.com>
2025-09-08 13:42:05 -0500

    Fixed technology loop caused by eva.

--- prototypes/technologies/eva.lua
+++ prototypes/technologies/eva.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalternativeenergygraphics__/graphics/technology/eva.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"renewable-mk02"},
+    prerequisites = {},
     effects = {},
     unit = {
         count = 250,
