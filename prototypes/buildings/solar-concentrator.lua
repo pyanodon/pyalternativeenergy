b5e1fd8323747af02b22b9b0f0ef88454e8b9485
protocol-1903 <fisher.braydon@yahoo.com>
2025-10-21 23:53:27 -0700

    Revert "Increased the output fluidbox size of all buildings from 100->1000. Allows fluid transfer at even higher rates before hitting engine limitations. Resolves https://github.com/pyanodon/pybugreports/issues/1203"
    This reverts commit 89628680679c73b95b7a8c24928bcdc45e0e7728.

--- prototypes/buildings/solar-concentrator.lua
+++ prototypes/buildings/solar-concentrator.lua
@@ -190,14 +190,14 @@ ENTITY {
             production_type = "output",
             pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
             pipe_covers = py.pipe_covers(false, true, true, true),
-            volume = 1000,
+            volume = 100,
             pipe_connections = {{flow_direction = "output", position = {-3, -6.0}, direction = defines.direction.north}}
         },
         {
             production_type = "output",
             pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
             pipe_covers = py.pipe_covers(false, true, true, true),
-            volume = 1000,
+            volume = 100,
             pipe_connections = {{flow_direction = "output", position = {2, 6.0}, direction = defines.direction.south}}
         },
     },
