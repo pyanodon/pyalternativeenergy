e191f45f7103b05239d43b3c249b2bbf30b8e2ba
protocol-1903 <67478786+protocol-1903@users.noreply.github.com>
2026-07-28 19:33:13 -0700

    mark fish farm turd building (#196)

--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -486,6 +486,9 @@ data.raw["assembling-machine"]["zipir-reef-mk02"].energy_usage = "2MW"
 data.raw["assembling-machine"]["zipir-reef-mk03"].energy_usage = "3MW"
 data.raw["assembling-machine"]["zipir-reef-mk04"].energy_usage = "4MW"
 
+-- new farm building
+py.mod_data.farm_buildings["fish-farm-turd"] = {default_module = "fish", domain = "animal"}
+
 --pySE
 if not mods["pystellarexpedition"] then return end
 data.raw["assembling-machine"]["arqad-hive-mk01-with-cags"].energy_usage = "1MW"
